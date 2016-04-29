d39507e6b9d733378054ab98a43bd4ea196f5e7c
jvahtre <jaan.vahtre@gmail.com>
2016-04-29 13:20:22 +0300

    Added files via upload

---
+++ Ent.ps1
@@ -0,0 +1,325 @@
+﻿<#
+.SYNOPSIS
+   Analysis script to determine whether a file is encrypted or not. 
+
+.NOTES
+   File Name: Ent.ps1
+   Author   : Jaan Vahtre
+   Version  : 1.0
+
+#>
+
+
+function FindEncrypted {
+
+#Start program stopwatch
+$stopwatch = [diagnostics.stopwatch]::StartNew()
+
+#Variables
+
+#Path Variables
+$DataPath = $Path + $Data
+$CryptPath = $Path + $Crypt
+$CleanPath = $Path + $Clean
+$LogPath = $Path + $SMBLog
+
+#HIT Paths
+$EntropyHitPath = $Path + $EntropyData
+$MeanHitPath = $Path + $MeanData
+$ChiHitPath = $Path + $ChiData
+$MonteHitPath = $Path + $MonteData
+$SerialHitPath = $Path + $SerialData
+
+#Clean Paths
+$EntropyCleanPath = $Path + $EntropyCleanData
+$MeanCleanPath = $Path + $MeanCleanData
+$ChiCleanPath = $Path + $ChiCleanData
+$MonteCleanPath = $Path + $MonteCleanData
+$SerialCleanPath = $Path + $SerialCleanData
+
+
+#Encrypted Serial can also be negative
+$MinusEncryptedSerial = -$EncryptedSerial
+
+#Set up the counters
+$SuspiciousEntropy = 0
+$SuspiciousChi = 0
+$SuspiciousMean = 0
+$SuspiciousMonte = 0
+$SuspiciousSerial = 0
+
+$EncryptedFile = 0
+$CleanCount = 0
+$CryptCount = 0
+$EntropyHit = 0
+$ChiHit = 0
+$SerialHit = 0
+$MeanHit = 0
+
+$Hit=0 
+$HitValue = 0
+
+
+#Setup start time and date.
+$StartTime = (Get-Date)
+
+#Add File Name to Both the Clean file list and cryped file list
+echo "Filename" > $CryptPath
+echo "Filename" > $CleanPath
+echo "Filename" > $EntropyHitPath
+echo "Filename" > $MeanHitPath
+echo "Filename" > $ChiHitPath
+echo "Filename" > $SerialHitPath
+echo "Filename" > $MonteHitPath
+echo "Filename" > $EntropyCleanPath
+echo "Filename" > $MeanCleanPath
+echo "Filename" > $ChiCleanPath
+echo "Filename" > $SerialCleanPath
+echo "Filename" > $MonteCleanPath
+
+#Clean Log File.
+echo " " > $LogFile
+
+    #Start Scanning Recursively
+    Get-ChildItem $SearchDirectory -Recurse -Exclude $ExcludedFiles |  % {
+
+    #Set FileName For each file
+
+    $FilePath = $_.FullName
+    $File = $_.Name
+
+
+   #Execution of Random Calculator Program
+    $Command = C:\'Powershell skriptid'\ENT\ent.exe -t $FilePath > $DataPath
+
+        #Import the results and display them as individual objects
+        Import-Csv $DataPath | foreach {
+
+            $Object = new-object PSObject -property @{
+
+
+            Filebytes = $_."File-bytes"
+
+            Entroopia = $_.Entropy
+
+            Chi = $_."Chi-square"
+
+            Mean = $_.Mean
+
+            Monte = $_."Monte-Carlo-Pi"
+
+            Serial = $_."Serial-Correlation"
+
+            } | Select Filebytes, Entroopia, Chi, Serial, Mean, Monte
+
+
+
+        #Trim  Mean,Monte and chi-square values 
+        $Mean = [math]::Truncate([double]$_.Mean * 10) / 10
+        $Monte = [math]::Truncate([double]$_."Monte-Carlo-Pi" * 100) / 100
+        $Chi = [math]::Truncate([double]$_."Chi-square" * 10) / 10
+        $Entropy = [math]::Truncate([double]$_."Entropy" * 1000000) / 1000000
+
+
+         #Check Entropy. If it's greater than 7.99984 setup the file as suspicious and add 1 to EncryptedFile flag.
+            if ($EvaluateEntropy) {
+
+            $HitValue++
+
+                if ($Entropy -ge $EncryptedEntropy) {
+
+                Write-Output $File >> $EntropyHitPath
+
+
+                $EncryptedFile++
+                $SuspiciousEntropy++
+                $EntropyHit = 1
+                $Hit++
+
+                }
+                else {
+
+                Write-Output $File >> $EntropyCleanPath
+
+                $EntropyHit = 0
+
+                }
+            }
+
+         #Serial Testing
+            if ($EvaluateSerial) {
+
+            $HitValue++
+
+                if ($_."Serial-Correlation" -ge "$MinusEncryptedSerial" -Or $_."Serial-Correlation" -ge "$EncryptedSerial") { 
+
+                #Clean File
+
+                Write-Output $File >> $SerialCleanPath
+
+
+                $SerialHit = 0
+                }
+                else {
+
+                Write-Output $File >> $SerialHitPath
+
+                $EncryptedFile++
+                $SuspiciousSerial++
+                $SerialHit = 1
+                $Hit++
+                }
+            }
+
+            #Mean Testing
+            if ($EvaluateMean) {
+
+            $HitValue++
+
+                if ($Mean -ge $LowestEncryptedMean -And $Mean -le $HighestEncryptedMean -or $Mean -eq $EncryptedMean ) {
+
+                Write-Output $File >> $MeanHitPath
+
+                $EncryptedFile++
+                $SuspiciousMean++
+                $MeanHit = 1
+                $Hit++
+
+                }
+                else {
+
+                Write-Output $File >> $MeanCleanPath
+
+                $MeanHit = 0
+                }
+            }
+
+            #Monte Testing
+            if ($EvaluateMonte) {
+
+            $HitValue++
+
+                if ($Monte -eq $EncryptedMonte -or $Monte -ge $LowestEncryptedMonte -And $Monte -le $HighestEncryptedMonte ) {
+
+                Write-Output $File >> $MonteHitPath
+
+                $EncryptedFile++
+                $SuspiciousMonte++
+                $MonteHit = 1
+                $Hit++
+                }
+                else {
+
+                Write-Output $File >> $MonteCleanPath
+
+                $MonteHit = 0
+                }
+            }
+
+            #Chi-square Testing
+            if ($EvaluateChi) {
+
+            $HitValue++
+
+                if ($Chi -ge $EncryptedChi) {
+
+                #The file is not encrypted
+                $ChiHit = 0
+
+                Write-Output "$File" >> $ChiCleanPath
+                }
+                #Otherwise it's encrypted
+                else {
+
+                Write-Output "$File" >> $ChiHitPath
+
+
+                $EncryptedFile++
+                $SuspiciousChi++
+                $ChiHit = 1
+                $Hit++
+
+                }
+            }
+
+            if ($Hit -ge $HitValue) {
+
+            $CryptCount++
+
+            EncryptedFileFound
+            DisableUser
+
+            $ChiHit = 0
+            $MonteHit = 0
+            $SerialHit = 0
+            $EntropyHit = 0
+            $MeanHit = 0
+
+            $EncryptedFile = 0
+            $Hit = 0
+            $HitValue = 0
+
+            }
+
+            else {
+
+            #Pure File - Send the data to CleanPath File.
+            Write-Output "Clean File" >> $CleanPath
+            Write-Output "$File" >> $CleanPath
+            Write-Output $Object >> $CleanPath
+            Write-Output "==================================================== " >> $CleanPath
+            $CleanCount++
+            $EncryptedFile = 0
+            $Hit= 0
+            $HitValue = 0
+
+            }
+        }
+    }
+
+$TotalFiles = $CleanCount + $CryptCount
+
+
+#Set the ending time and the total time it took to run the test.
+
+$EndTime = (Get-Date)
+$RunTime = New-TimeSpan -Start $StartTime -End $EndTime
+Write-Output "The Running of the Script ended at: $EndTime" >> $CleanPath
+Write-Output "The Running of the Script ended at: $EndTime" >> $CryptPath
+
+
+#Stop the stopwatch.
+
+$stopwatch.Stop()
+$Stopwatchtime = [math]::Truncate([double]$stopwatch.Elapsed.TotalSeconds * 100) / 100
+
+Write-Output "Total time was $Stopwatchtime seconds" >> $CryptPath
+Write-Output "Total time was $Stopwatchtime seconds" >> $CleanPath
+Write-Output "===================================================== " >> $CryptPath
+Write-Output "===================================================== " >> $CleanPath
+
+LogWrite "                                                      "
+LogWrite "===================================================== " 
+LogWrite "Summary:"
+LogWrite "===================================================== " 
+LogWrite "Total Files: $TotalFiles"
+LogWrite "Clean Files: $CleanCount"
+LogWrite "Encrypted Files: $CryptCount"
+LogWrite "Excluded Files: $ExcludedFileCount"
+LogWrite "===================================================== "
+LogWrite "===================================================== "
+LogWrite "Overall Statistics:"
+LogWrite "===================================================== "
+LogWrite "Entropy Hits: $SuspiciousEntropy"
+LogWrite "Mean Hits: $SuspiciousMean"
+LogWrite "Monte Hits: $SuspiciousMonte"
+LogWrite "Chi Hits: $SuspiciousChi"
+LogWrite "Serial Hits: $SuspiciousSerial"
+LogWrite "===================================================== " 
+LogWrite "Running of the script took: $Stopwatchtime seconds"
+
+
+}
+
+
+
---
+++ FindEncryptedFiles.ps1
@@ -0,0 +1,104 @@
+﻿<#
+.SYNOPSIS
+   Checks for Encrypted Files in a specified folder and logs off the offending user, when a encrypted file is found. Uses ENT.ps1 and functions.ps1. 
+
+.DESCRIPTION
+   Checks for Encrypted Files in a specified folder and logs off the offending user, when a encrypted file is found. Uses ENT.ps1 and functions.ps1.
+
+.NOTES
+   File Name: FindEncryptedFiles.ps1
+   Author   : Jaan Vahtre
+   Version  : 1.0
+
+.EXAMPLE
+   PS > .\FindEncryptedFiles.ps1
+#>
+
+
+#In order to run the script, The necessary variables need to be entered.
+
+
+#Import functions script and ENT Program script.
+ . "PATHTO\functions.ps1"
+ . "PATHTO\ENT.ps1"
+
+#Set the Variables
+
+#SentMail LogFile
+$Logfile = "PATHTO\SentMail.log"
+
+#First Define the Search Directory
+$SearchDirectory = "PATHTOSEARCHDIRECTORY"
+
+#Setup the Path where ENT tool is located.
+$Path = "PATHTOENTTOOLLOCATION" 
+
+#Setup where the .csv output will be sent and read.
+$Data = "Data.txt"
+
+#Setup the path of Encrypted files log.
+$Crypt = "Crypted.txt"
+
+#Setup the path of Clean files log.
+$Clean = "Clean.txt"
+
+#Setup the path for SMB Logs.
+$SMBLog = "Log.txt"
+
+#Setup the FileNames of suspicious files log.
+$EntropyData = "EntropyHit.xlsx"
+$MeanData = "MeanHit.xlsx"
+$ChiData = "ChiHit.xlsx"
+$SerialData = "SerialHit.xlsx"
+$MonteData = "MonteHit.xlsx"
+
+#Setup the FileNames of Clean files log.
+$EntropyCleanData = "CleanEntropy.xlsx"
+$MeanCleanData = "CleanMean.xlsx"
+$ChiCleanData = "CleanChi.xlsx"
+$SerialCleanData = "CleanSerial.xlsx"
+$MonteCleanData = "CleanMonte.xlsx"
+
+
+#Excluded Files list. 
+
+$ExcludedFileCount = 0
+$ExcludedFiles=@("")
+
+#Set Whether Users are disconnected or not when encryption is found.
+$Disconnect = $False
+
+#Set Whether the algorithm will be evaluated
+$EvaluateEntropy = $False
+$EvaluateChi = $False
+$EvaluateMean = $False
+$EvaluateSerial = $False
+$EvaluateMonte = $False
+
+#Set the Fileshare name, from which the User will be disconnected. If not specified, the user will be disconnected from every share. 
+$FileShareName = "FILESHARENAME"
+
+
+#Set the Encrypted Values for entropy, chi-square, Monte, Serial Correlation and Mean. Optimize in order to get better results, 
+$EncryptedEntropy = 7.986000
+$EncryptedChi = 690
+$EncryptedMean = 127.5
+$LowestEncryptedMean = 127.3
+$HighestEncryptedMean = 127.7
+$LowestEncryptedMonte = 3.12
+$HighestEncryptedMonte = 3.16
+$EncryptedMonte = 3.14
+$EncryptedSerial = 0.001200
+
+#Start finding the Encrypted Files. The timeout is 1440 minutes, but can be adjusted accordingly. 
+#In addition, it's possible to add a task scheduler script to activate it in Every 1440 Minutes.
+
+$timeout = new-timespan -Minutes 1440
+$sw = [diagnostics.stopwatch]::StartNew()
+
+while ($sw.elapsed -lt $timeout){
+    FindEncrypted
+    start-sleep -seconds 5
+}
+write-host "Program Closed"
+
